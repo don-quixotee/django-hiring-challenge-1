95c1b1f1ab89846a6cd82dc8a8ff0f63b126f4f6
don-quixotee <Abhishek>
2020-07-26 20:38:18 +0530

    pg database setup

--- crawlBot/settings.py
+++ crawlBot/settings.py
@@ -82,13 +82,6 @@ WSGI_APPLICATION = 'crawlBot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 
 DATABASES = {
@@ -102,6 +95,13 @@ DATABASES = {
 
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
+
 
 
 
